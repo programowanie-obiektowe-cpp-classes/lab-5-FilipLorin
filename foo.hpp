8ccdac861e36e1d11939de3f52017c23137630b3
Mr-Moose-dev <filip.lorin@gmail.com>
2024-11-25 19:27:40 +0100

    solved

--- foo.hpp
+++ foo.hpp
@@ -8,5 +8,14 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     // Twoja implementacja tutaj
-    return {};
+    std::vector<char> output;
+
+    for(auto it = people.rbegin(); it != people.rend(); it++) {
+        it->birthday();
+        if(it->isMonster())
+            output.push_back('n');
+        else
+            output.push_back('y');
+    }
+    return output;
 }
